f3274323d13d19972618288b01c41a5d18d0d015
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2021-08-22 10:03:02 +0600

    final commit

--- js/script.js
+++ js/script.js
@@ -6,7 +6,9 @@ function calculateTotal() {
     const ramCostNum = parseFloat(ramCost.innerText);
     const ssdCostNum = parseFloat(ssdCost.innerText);
     const deliveryCostNum = parseFloat(deliveryCost.innerText);
+    const discountPriceId = document.getElementById('discount-price');
     totalPrice.innerText = ramCostNum + ssdCostNum + deliveryCostNum + 1299;
+    discountPriceId.innerText = ramCostNum + ssdCostNum + deliveryCostNum + 1299;
 }
 
 document.getElementById('eight-gb-ram').addEventListener('click', function() {
@@ -52,3 +54,16 @@ document.getElementById('delivery-method-2').addEventListener('click', function(
     deliveryCost.innerText = 20;
     calculateTotal();
 });
+
+document.getElementById('promo-button').addEventListener('click', function() {
+    const promoInput = document.getElementById('promo-input');
+    const discountPriceId = document.getElementById('discount-price');
+    const promoCode = promoInput.value;
+    const totalPrice = document.getElementById('total-price');
+    const totalPriceNum = parseFloat(totalPrice.innerText);
+    if (promoCode == "stevekaku") {
+        const discountPrice = totalPriceNum - totalPriceNum * (20 / 100);
+        discountPriceId.innerText = discountPrice;
+    }
+
+})
