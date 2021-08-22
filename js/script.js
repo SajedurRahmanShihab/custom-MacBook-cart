66f362d4f871f7e9e3539fe86f6e2c71d1039bf2
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2021-08-22 09:46:21 +0600

    total price completed

--- js/script.js
+++ js/script.js
@@ -1,39 +1,54 @@
-// Ram price
+function calculateTotal() {
+    const totalPrice = document.getElementById('total-price');
+    const ramCost = document.getElementById('ram-cost');
+    const ssdCost = document.getElementById('ssd-cost');
+    const deliveryCost = document.getElementById('delivery-cost');
+    const ramCostNum = parseFloat(ramCost.innerText);
+    const ssdCostNum = parseFloat(ssdCost.innerText);
+    const deliveryCostNum = parseFloat(deliveryCost.innerText);
+    totalPrice.innerText = ramCostNum + ssdCostNum + deliveryCostNum + 1299;
+}
+
 document.getElementById('eight-gb-ram').addEventListener('click', function() {
     const ramCost = document.getElementById('ram-cost');
-    ramCost.innerHTML = '0';
+    ramCost.innerText = 0;
+    calculateTotal();
 });
 
 document.getElementById('sixteen-gb-ram').addEventListener('click', function() {
     const ramCost = document.getElementById('ram-cost');
-    ramCost.innerHTML = '180';
+    ramCost.innerText = 180;
+    calculateTotal();
 });
 
 
-// ssd price
 document.getElementById('ssd-256').addEventListener('click', function() {
-    const ramCost = document.getElementById('ssd-cost');
-    ramCost.innerHTML = '0';
+    const ssdCost = document.getElementById('ssd-cost');
+    ssdCost.innerText = 0;
+    calculateTotal();
 });
 
 document.getElementById('ssd-512').addEventListener('click', function() {
-    const ramCost = document.getElementById('ssd-cost');
-    ramCost.innerHTML = '100';
+    const ssdCost = document.getElementById('ssd-cost');
+    ssdCost.innerText = 100;
+    calculateTotal();
 });
 
 document.getElementById('ssd-1TB').addEventListener('click', function() {
-    const ramCost = document.getElementById('ssd-cost');
-    ramCost.innerHTML = '180';
+    const ssdCost = document.getElementById('ssd-cost');
+    ssdCost.innerText = 180;
+    calculateTotal();
 });
 
 
-// delivery method
 document.getElementById('delivery-method-1').addEventListener('click', function() {
-    const ramCost = document.getElementById('delivery-cost');
-    ramCost.innerHTML = '0';
+    const deliveryCost = document.getElementById('delivery-cost');
+    deliveryCost.innerText = 0;
+    calculateTotal();
 });
 
 document.getElementById('delivery-method-2').addEventListener('click', function() {
-    const ramCost = document.getElementById('delivery-cost');
-    ramCost.innerHTML = '20';
+    const deliveryCost = document.getElementById('delivery-cost');
+    deliveryCost.innerText = 20;
+    calculateTotal();
 });
